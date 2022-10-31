57905a150da87f2d136490d912828d22af241a5d
Julian Ewald <j.ewald97@gmail.com>
2022-10-31 12:58:13 +0100

    Added save/load functionality. Players can save the game every round. Loading is possible at the start of the game.

--- hangman.rb
+++ hangman.rb
@@ -1,110 +1,170 @@
+require 'yaml'
+
 def clear
   print "\e[2J\e[H"
 end
 
-def load_graphics
-  filename = 'graphics.txt'
-  lines = File.open(filename).to_a
-  graphics = Array.new(7, '')
-  start_line = 0
+class Game
+  def initialize
+    @player_won = false
+    @graphics = load_graphics
+    @used_letters = []
+    @wrong_guesses = []
+    @word_to_guess = grab_random_word.split('')
+    @correct_guesses = Array.new(@word_to_guess.length, '_')
+  end
 
-  graphics.each_index do |index|
-    for i in 0..5
-      graphics[index] = graphics[index] + lines[i + start_line]
+  def load_graphics
+    filename = 'graphics.txt'
+    lines = File.open(filename).to_a
+    @graphics = Array.new(7, '')
+    start_line = 0
+
+    @graphics.each_index do |index|
+      for i in 0..5
+        @graphics[index] = @graphics[index] + lines[i + start_line]
+      end
+      start_line += 6
     end
-    start_line += 6
+    @graphics
   end
-  graphics
-end
 
-def grab_random_word
-  word = ""
-  filename = 'dictionary.txt'
-  lines = File.open(filename).to_a
-  line_count = lines.count
-
-  until word.length >= 5 && word.length <= 12
-    random_line = rand(0..line_count-1)
-    word = lines[random_line].chomp
+  def choose_gamemode
+    if Dir.empty?('saves')
+      return
+    else
+      puts "1: New Game\n2: Load Game"
+      print 'Choose your game mode: '
+      gamemode = gets.chomp.to_s
+      load_game if gamemode == '2'
+    end
   end
-  word
-end
 
-def user_guess(used_letters, word_to_guess)
-  guess = gets.chomp
+  def load_game
+    clear
+    savefiles = Dir.entries('saves').sort
+    savefiles = savefiles[2..]
+    savefiles.each_with_index do |file, index|
+      puts "#{index + 1}: #{file}"
+    end
+    print "\nEnter number of savegame to load: "
+    save_number = gets.chomp.to_i
 
-  if used_letters.include?(guess)
-    print "You guessed this before.\nEnter your guess: "
-    user_guess(used_letters, word_to_guess)
+    savestate = YAML.load_file("./saves/#{savefiles[save_number - 1]}")
+    @used_letters = savestate['used_letters']
+    @wrong_guesses = savestate['wrong_guesses']
+    @word_to_guess = savestate['word_to_guess']
+    @correct_guesses = savestate['correct_guesses']
+  end
 
-  elsif guess.empty?
+  def grab_random_word
+    word = ""
+    filename = 'dictionary.txt'
+    lines = File.open(filename).to_a
+    line_count = lines.count
+
+    until word.length >= 5 && word.length <= 12
+      random_line = rand(0..line_count-1)
+      word = lines[random_line].chomp
+    end
+    word
+  end
+
+  def user_guess
+    puts "\nEnter \"1\" to save and exit the game."
     print 'Enter your guess: '
-    user_guess(used_letters, word_to_guess)
 
-  else
-    used_letters.push(guess)
-    guess
-  end
-end
+    @guess = gets.chomp.downcase
 
-def compare(guess, word_to_guess, correct_guesses, wrong_letters)
-  if guess == word_to_guess.join('')
-    word_to_guess.each_with_index { |letter, index| correct_guesses[index] = letter }
+    if @guess == '1'
+      save_game
 
-  elsif word_to_guess.include?(guess)
-    word_to_guess.each_with_index do |letter, index|
-      correct_guesses[index] = guess if letter == guess
+    elsif @used_letters.include?(@guess)
+      print "You guessed this before.\nEnter your guess: "
+      user_guess
+
+    elsif @guess.empty?
+      print 'Enter your guess: '
+      user_guess
+
+    else
+      @used_letters.push(@guess)
+      @guess
     end
-  else
-    wrong_letters.push(guess)
-  end
-end
-
-def play_game
-  player_won = false
-  graphics = load_graphics
-  guesses = 0
-  used_letters = []
-  wrong_letters = []
-  guessed_letters = []
-  word_to_guess = grab_random_word.split('')
-  correct_guesses = Array.new(word_to_guess.length, '_')
-
-  puts "#{graphics[0]}\n"
-
-  puts "#{correct_guesses.join(' ')}\n\nYour previous incorrect guesses:\n"
-
-  while player_won == false && wrong_letters.count < 6 do
-
-    print "\nEnter your guess: "
-    guess = user_guess(used_letters, word_to_guess)
-
-    clear
-
-    compare(guess, word_to_guess, correct_guesses, wrong_letters)
-    print "#{graphics[wrong_letters.count]} \n"
-
-    puts correct_guesses.join(' ')
-    puts "\n"
-    puts "Your previous incorrect guesses: #{wrong_letters.join(', ')}"
-    player_won = true if word_to_guess.join == correct_guesses.join
   end
 
-  if player_won
-    puts 'Congrats, you won this round!'
-  else
+  def compare
+    if @guess == @word_to_guess.join('')
+      @word_to_guess.each_with_index { |letter, index| @correct_guesses[index] = letter }
+
+    elsif @word_to_guess.include?(@guess)
+      @word_to_guess.each_with_index do |letter, index|
+        @correct_guesses[index] = @guess if letter == @guess
+      end
+    else
+      @wrong_guesses.push(@guess)
+    end
+  end
+
+  def play_game
+    choose_gamemode
     clear
-    puts graphics[6]
-    puts "\n#{correct_guesses.join(' ')}\n"
-    puts "\nYour previous incorrect guesses: #{wrong_letters.join(', ')}"
-    puts "\nYou didn't win this time. The word we were looking for was \"#{word_to_guess.join}\". Try again!"
+
+    puts "#{@graphics[@wrong_guesses.count]}\n"
+
+    puts "#{@correct_guesses.join(' ')}\n\nYour previous incorrect guesses: #{@wrong_guesses.join(', ')}\n"
+
+    while @player_won == false && @wrong_guesses.count < 6 do
+
+      @guess = user_guess
+
+      clear
+
+      compare
+      print "#{@graphics[@wrong_guesses.count]} \n"
+
+      puts @correct_guesses.join(' ')
+      puts "\n"
+      puts "Your previous incorrect guesses: #{@wrong_guesses.join(', ')}"
+      @player_won = true if @word_to_guess.join == @correct_guesses.join
+    end
+
+    if @player_won
+      puts 'Congrats, you won this round!'
+    else
+      clear
+      puts @graphics[6]
+      puts "\n#{@correct_guesses.join(' ')}\n"
+      puts "\nYour previous incorrect guesses: #{@wrong_guesses.join(', ')}"
+      puts "\nYou didn't win this time. The word we were looking for was \"#{@word_to_guess.join}\". Try again!"
+    end
+  end
+
+  def create_save
+    {
+      'used_letters' => @used_letters,
+      'wrong_guesses' => @wrong_guesses,
+      'word_to_guess' => @word_to_guess,
+      'correct_guesses' => @correct_guesses
+    }
+  end
+
+  def save_game
+    print "Enter a name for your save file: "
+    filename = gets.chomp.to_s
+    File.open("./saves/#{filename}.yml", 'w') { |file| file.write(create_save.to_yaml) }
+    puts 'Game saved. Press enter to exit game.'
+    gets
+    exit
   end
 end
 
 play_on = true
-while play_on == true do
+
+while play_on == true
+  game = Game.new
   clear
-  play_game
+  game.play_game
   puts "\nPress Enter to start a new game. Enter \"q\" to quit."
   play_on = false if gets.chomp == 'q'
 end
